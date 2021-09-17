dec6b5661b408475213c82f3357f50db4bbb5105
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-09-17 15:15:28 -0600

    added pk to lines class

--- src/tables/tbllines.py
+++ src/tables/tbllines.py
@@ -1,24 +1,7 @@
 from src.utils.database_functions import arcno
+from src.primarykeys.primary_key_functions import pk_appender
 import os
 import pandas as pd
-
-# no primary key: tblLines, tblPlots, tblSpecies
-no_primary_key = ['tblPlots', 'tblLines', 'tblSpecies','tblSpeciesGeneric',\
-                      'tblSites','tblPlotNotes', 'tblSites']
-
-
-p = r"C:\Users\kbonefont\Downloads\dimas\DONE MCC Mongolia Fall Production 2017 DIMA 5.5a as of 2020-06-26.mdb"
-dimadir = os.path.dirname(p)
-dimalist = os.listdir(dimadir)
-p1 = os.path.join(dimadir,dimalist[0])
-p2 = os.path.join(dimadir,dimalist[1])
-
-pp
-
-
-
-
-
 
 
 class Lines:
@@ -27,9 +10,13 @@ class Lines:
         self._dimapath = dimapath
         self._table_name = "tblLines"
         self.raw_table = arcno.MakeTableView("tblLines", dimapath)
+        self.lines_pk = self.line_pk(3)
 
-    def lines_table_fixes(self):
-        if 'DateLoaded'
+    def line_pk(self, custom_daterange):
+        # join to pk source with
+        self.pk_source = pk_appender(self._dimapath, custom_daterange)
+        return pd.merge(self.raw_table, self.pk_source.loc[:,['PlotKey','PrimaryKey']], how="inner", on="PlotKey")
+
 
 class MultipleLines:
 
@@ -37,7 +24,6 @@ class MultipleLines:
         self.lines_dictionary = {f"list_{i}":Lines(os.path.join(dimadir,dimalist[i])).raw_table for i in range(0,len(dimalist))}
         self.raw_table = pd.concat([j for i,j in self.lines_dictionary.items()],ignore_index=True)
 
-MultipleLines(dimadir).raw_table.drop_duplicates
 
 
 

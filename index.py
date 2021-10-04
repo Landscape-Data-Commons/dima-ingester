1ba19458df47e2af2523841dcb1aefdca17c0b1f
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-10-04 14:36:01 -0600

    check for projectfile; batch report added

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from cmd import Cmd
 import logging
 from src.project.project import all_dimas, update_project
 from src.utils.batch_utilities import batch_looper, table_collector
+from src.utils.report import csv_report
 from src.tables.tbllines import Lines
 from src.utils.batch_utilities import table_operations, looper
 
@@ -41,26 +42,39 @@ class main(Cmd):
 
     def do_report(self,args):
         """
-        primarykey report
+        Create a csv with primary keys and dbkeys for each table.
+        If 'all' is used as the table argument, will print out a csv report per table
+        inside the dima directory.
         """
         table, pk_formdate_range = args.split(" ", 3)
-        df = looper(self.batch_path, table,"report_table",pk_formdate_range )
-        if "PrimaryKey" in df['dataframe'].columns:
-            pk_list = df['dataframe'].PrimaryKey.unique()
-            small_df = df['dataframe'].loc[:4,[i for i in df['dataframe'].columns if 'Key' in i]]
-            small_df['DBKey'] = small_df['DBKey'].apply(lambda x: x[:15])
+        tablelist = table_collector(self.batch_path)
+        if table in tablelist:
+            df = looper(self.batch_path, table,"report_table",pk_formdate_range )
+            if "PrimaryKey" in df['dataframe'].columns:
+                pk_list = df['dataframe'].PrimaryKey.unique()
+                print("******************")
+                print(f"number of primarykeys created with Formdate classes every '{pk_formdate_range}' days ")
+                print("******************")
+                print(f"list of PrimaryKey values using '{pk_formdate_range}' for table '{table}':")
+                print(pk_list)
+                print("******************")
+                csv_report(df, table, self.batch_path)
+                print("******************")
+            else:
+                print(f"No PrimaryKey field created for '{table}'")
+        elif 'all' in table:
+            revised_tablelist = [i for i in tablelist if i not in ['tblSites','tblSpecies','tblSpeciesGeneric', 'tblPlotNotes', 'tblPlotHistory']]
+            print("creating a report for each table...")
             print("******************")
-            print(f"number of primarykeys created with Formdate classes every '{pk_formdate_range}' days ")
-            print(len(small_df.PrimaryKey.unique()))
-            print("******************")
-            print(f"list of PrimaryKey values using '{pk_formdate_range}' for table '{table}':")
-            print(pk_list)
-            print("******************")
-            print(f"snippet of table '{table}':")
-            print(small_df)
-            print("******************")
+            for i in revised_tablelist:
+                print(f"creating csv report for {}..")
+                df = looper(self.batch_path, i,"report_table",pk_formdate_range )
+                pk_list = df['dataframe'].PrimaryKey.unique()
+                csv_report(df, table, self.batch_path)
+                print("******************")
         else:
-            print(f"No PrimaryKey field created for '{table}'")
+            print("Not implemented.")
+
 
     def do_ingest(self,args):
         """ ingest all tables contained across all the dimas inside the dima
@@ -85,8 +99,11 @@ class main(Cmd):
             True:"dimadev",
             False:"dima"
             }
-        batch_looper(self.batch_path, projectkey, dev_or_not, pk_formdate_range)
-        update_project(self.batch_path, projectkey, dev_obj)
+        if any([True for i in os.listdir(dir) if '.xlsx' in os.path.splitext(i)[1]]):
+            batch_looper(self.batch_path, projectkey, dev_or_not, pk_formdate_range)
+            update_project(self.batch_path, projectkey, dev_obj)
+        else:
+            print("No project file found within dima directory; unable to ingest.")
 
     def do_exit(self, args):
         raise SystemExit()
